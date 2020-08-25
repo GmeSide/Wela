2162f2144c28a480e9c65a1a57f58d1b215c8764
nustony <tony@nustechnology.com>
2020-08-25 10:18:03 +0700

    Fix bug freezes on iOS

--- src/screens/venue/ManuallyAddQueue.js
+++ src/screens/venue/ManuallyAddQueue.js
@@ -76,7 +76,7 @@ export default class ManuallyAddQueue extends React.Component {
                     this.setState({ contact: [{ name: '', phone: '' }], personsCount: 1 })
                     showToastMessage("Create Account", jsonObject.apiResponse.message)
                     // this.props.navigation.navigate('VerifyOTP', { user: jsonObject.apiResponse })
-                    this.props.onAddManualQueueRequest()
+                    setTimeout(() => { this.props.onAddManualQueueRequest() }, 200);
                 }
             })
         }
